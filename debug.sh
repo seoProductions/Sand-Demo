9c7afcaca7be9063826db8f6e670b51c260ecc29
seoProductions <duque.eliseo.7@gmail.com>
2024-05-16 18:29:13 -0500

    Optimized

---
+++ debug.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+g++ -o -g run driver.cpp \
+              collision.h \
+              collision.cpp \
+              particles.h   \
+              particles.cpp \
+              -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
+
+# run if compiled
+if [ -f run ]
+then
+{
+  echo "compiled successfully, starting Sand-Demo"
+  chmod +x run
+         ./run
+
+}
+else
+  echo "ERROR: couldnt  compile"
+fi
+
